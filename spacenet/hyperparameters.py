bc45cfbe64c9c57f716c614923d35d6fa0659508
James W. McClain <jmcclain@azavea.com>
2018-11-09 11:22:10 -0500

    Allow `baseLearningRate` Hyperparameter to Vary

---
+++ spacenet/hyperparameters.py
@@ -0,0 +1,87 @@
+import os
+
+import rastervision as rv
+from spacenet.vegas import (SpacenetConfig, build_dataset, build_task,
+                            str_to_bool, validate_options, BUILDINGS)
+
+
+def build_backend(task, test, learning_rate):
+    if test:
+        debug = True
+    else:
+        debug = False
+
+    if not test:
+        batch_size = 12
+        num_steps = 1e5
+    else:
+        num_steps = 1
+        batch_size = 1
+
+    backend = rv.BackendConfig.builder(rv.TF_DEEPLAB) \
+                              .with_task(task) \
+                              .with_model_defaults(rv.MOBILENET_V2) \
+                              .with_num_steps(num_steps) \
+                              .with_batch_size(batch_size) \
+                              .with_config({'baseLearningRate': str(learning_rate)}) \
+                              .with_debug(debug) \
+                              .build()
+
+    return backend
+
+
+class HyperParameterSearch(rv.ExperimentSet):
+    def exp_main(self, root_uri, use_remote_data=True, test=False):
+        """Run an experiment on the Spacenet Vegas road or building dataset.
+
+        This is an example of how to do all three tasks on the same dataset.
+
+        Args:
+            root_uri: (str): root of where to put output
+            use_remote_data: (bool or str) if True or 'True', then use
+                data from S3, else local
+            test: (bool or str) if True or 'True', run a very small
+                experiment as a test and generate debug output
+
+        """
+        target = BUILDINGS
+        task_type = rv.SEMANTIC_SEGMENTATION
+
+        test = str_to_bool(test)
+        task_type = task_type.upper()
+        use_remote_data = str_to_bool(use_remote_data)
+        spacenet_config = SpacenetConfig.create(use_remote_data, target)
+        experiment_id = '{}_{}'.format(target, task_type.lower())
+        chip_uri = os.path.join(root_uri, experiment_id, 'chip')
+        analyze_uri = os.path.join(root_uri, experiment_id, 'analyze')
+
+        validate_options(task_type, target)
+
+        task = build_task(task_type, spacenet_config.get_class_map())
+        analyzer = rv.AnalyzerConfig.builder(rv.STATS_ANALYZER) \
+                                    .build()
+        dataset = build_dataset(task, spacenet_config, test)
+
+        retval = []
+        for learning_rate in ['0.001', '0.002', '0.005', '0.10']:
+            backend = build_backend(task, test, learning_rate)
+            experiment_id = '{}_{}_rate={}'.format(target, task_type.lower(), learning_rate)
+
+            # Need to use stats_analyzer because imagery is uint16.
+            experiment = rv.ExperimentConfig.builder() \
+                                            .with_id(experiment_id) \
+                                            .with_task(task) \
+                                            .with_backend(backend) \
+                                            .with_analyzer(analyzer) \
+                                            .with_dataset(dataset) \
+                                            .with_root_uri(root_uri) \
+                                            .with_analyze_uri(analyze_uri) \
+                                            .with_chip_uri(chip_uri)
+
+            retval.append(experiment.build())
+
+        return retval
+
+
+if __name__ == '__main__':
+    rv.main()
